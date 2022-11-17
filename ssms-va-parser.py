5a6151d947f2320811219faf4c686dca701f096a
Aidan Marlin <0ef4bbef-aacf-444c-ba58-bfa18b621f9d@gmail.com>
2022-11-17 11:28:38 +0000

    Testing

--- ssms-va-parser.py
+++ ssms-va-parser.py
@@ -6,6 +6,7 @@ from string import Template
 
 import argparse
 import csv
+import glob
 import os
 import sys
 
@@ -31,75 +32,106 @@ def welcome():
   print()
 
 def main():
-    parser = argparse.ArgumentParser(description="Parse SSMS CSV to output issues based on template file.")
-    parser.add_argument("--csv", required=True,
-                        help="SSMS CSV file")
-    parser.add_argument("--template", required=True,
-                        help="Template to build issues from")
-    parser.add_argument("--cvss", required=False, type=float, default=0, choices=[Range(0.0, 10.0)],
-                        help="Only include issues with this rating or above (default: 0)")
-    parser.add_argument("--output", required=False, default="nessus",
-                        help="Output directory to create issues (default: nessus)")
+  parser = argparse.ArgumentParser(description="Parse SSMS CSV directory to output issues based on template file.")
+  parser.add_argument("--csv-directory", required=True,
+                      help="SSMS CSV directory")
+  parser.add_argument("--template", required=True,
+                      help="Template to build issues from")
+  parser.add_argument("--cvss", required=False, type=float, default=0, choices=[Range(0.0, 10.0)],
+                      help="Only include issues with this rating or above (default: 0)")
+  parser.add_argument("--output", required=False, default="ssms-output",
+                      help="Output directory to create issues (default: ssms-output)")
 
-    parsed = parser.parse_args()
+  parsed = parser.parse_args()
 
-    # Open template
-    template_string = Path(parsed.template).read_text()
+  # Open template
+  template_string = Path(parsed.template).read_text()
 
-    # Open CSV
-    csv_file = csv.DictReader(open(parsed.csv))
-    for row in csv_file:
-      print(row)
+  # List all *.csv in target directory
+  # All files and directories ending with .txt and that don't begin with a dot:
+  csv_files = glob.glob(parsed.csv_directory + "/*.csv")
+  if len(csv_files) == 0:
+    print("ERROR: No CSV files identified, bailing..")
     sys.exit(1)
-    dedupe_dict = {}
-    for row in csv_file:
-      if row['Plugin ID'] not in dedupe_dict:
-        dedupe_dict[row['Plugin ID']] = dict(row)
-        # Hosts list
-        dedupe_dict[row['Plugin ID']]['Hosts'] = []
-      if row['Host'] not in dedupe_dict[row['Plugin ID']]['Hosts']:
-        dedupe_dict[row['Plugin ID']]['Hosts'].append(row['Host'])
 
-    # Check / create output directory
-    if os.path.exists(parsed.output):
-      print("ERROR: Output directory exists, bailing..")
-      sys.exit(1)
-    os.mkdir(parsed.output)
+  init_dict = {}
+  for csv_file in csv_files:
+    csv_data = csv.DictReader(open(csv_file))
+    init_dict[csv_file] = csv_data
 
-    # For each issue in dedupe dict
-    for issue in dedupe_dict:
-      risk_rating = dedupe_dict[issue]['CVSS v3.0 Base Score']
-      if risk_rating == "":
-        risk_rating = 0
+  dedupe_list = []
+  servers = {}
+  for csv_file in init_dict:
+    for row in init_dict[csv_file]:
+      try:
+        servers[row['ID']].append(row['Server'])
+      except:
+        servers[row['ID']] = []
+        servers[row['ID']].append(row['Server'])
+      row['Server'] = []
+      if row['ID'] != "" and row not in dedupe_list:
+        dedupe_list.append(row)
 
-      # Don't include issue if CVSS score too low
-      if float(risk_rating) < parsed.cvss:
-        continue
-      temp_obj = Template(template_string)
-      # Plugin ID,CVE,CVSS v2.0 Base Score,Risk,Host,Protocol,Port,Name,Synopsis,Description,Solution,See Also,Plugin Output,STIG Severity,CVSS v3.0 Base Score
-      # CVSS v2.0 Temporal Score,CVSS v3.0 Temporal Score,Risk Factor,BID,XREF,MSKB,Plugin Publication Date,Plugin Modification Date,Metasploit,Core Impact,CANVAS
-      if len(dedupe_dict[issue]['CVE']) == 0:
-        dedupe_dict[issue]['CVE'] = "N/A"
-      if len(dedupe_dict[issue]['Plugin Output']) == 0:
-        dedupe_dict[issue]['Plugin Output'] = "N/A"
-      issue_file = open(parsed.output + "/" + issue + ".tex", "w")
-      issue_file.write(
-        temp_obj.substitute(
-          plugin_id=dedupe_dict[issue]['Plugin ID'],
-          cvss3=dedupe_dict[issue]['CVSS v3.0 Base Score'],
-          risk=dedupe_dict[issue]['Risk'],
-          name=dedupe_dict[issue]['Name'],
-          plugin_output=dedupe_dict[issue]['Plugin Output'],
-          synopsis=dedupe_dict[issue]['Synopsis'].replace("_","\_"),
-          description=dedupe_dict[issue]['Description'].replace("_","\_"),
-          # There must be a better way..
-          cve="        \item \\href{{https://cve.mitre.org/cgi-bin/cvename.cgi?name={0}}}{{{0}}}\n".format("".join(cve for cve in dedupe_dict[issue]['CVE'].split())),
-          host=''.join('        \item %s\n' % host for host in dedupe_dict[issue]['Hosts']),
-          see_also=''.join('%%        \\url{%s}\n\n' % see_also for see_also in dedupe_dict[issue]['See Also'].split()),
-          solution=dedupe_dict[issue]['Solution'].replace("_","\_"),
-        )
+  for row in dedupe_list:
+    row['Server'] = servers[row['ID']]
+
+  #for row in dedupe_list:
+  #  print(row)
+
+  # Check / create output directory
+  if os.path.exists(parsed.output):
+    print("ERROR: Output directory exists, bailing..")
+    #sys.exit(1)
+  #os.mkdir(parsed.output)
+
+#{
+#  "Status": "Fail",
+###  "Risk": "Medium",
+###  "ID": "VA1219",
+###  "Server": "SRVSG001",
+###  "Database": "SIGIA",
+#  "Applies to": "database",
+#  "Security Check": "Transparent data encryption should be enabled",
+###  "Description": "Transparent data encryption (TDE) helps to protect the database files against information disclosure by performing real-time encryption and decryption of the database, associated backups, and transaction log files 'at rest', without requiring changes to the application. This rule checks that TDE is enabled on the database.",
+#  "Category": "Data Protection",
+#  "Benchmark References": "FedRAMP",
+###  "Rule Query": "SELECT CASE WHEN EXISTS\n( SELECT *\n    FROM sys.databases\n    WHERE name = db_name()\n    AND is_encrypted = 0)\nTHEN 1\nELSE 0\nEND AS [Violation]",
+###  "Actual Result": "1",
+###  "Expected Result": "0",
+###  "Remediation": "Enable TDE on the affected database. Please follow the instructions on https://docs.microsoft.com/en-us/sql/relational-databases/security/encryption/transparent-data-encryption",
+###  "Remediation Script": ""
+#}
+
+  # For each issue in dedupe dict
+  for issue in dedupe_list:
+    #risk_rating = dedupe_dict[issue]['CVSS v3.0 Base Score']
+    #if risk_rating == "":
+    #  risk_rating = 0
+
+    # Don't include issue if CVSS score too low
+    #if float(risk_rating) < parsed.cvss:
+    #  continue
+    temp_obj = Template(template_string)
+
+    issue_file = open(parsed.output + "/" + issue['ID'] + ".tex", "w")
+    issue_file.write(
+      temp_obj.substitute(
+        risk=issue['Risk'],
+        name=issue['Security Check'].replace("_","\_"),
+        #plugin_output=issue['Plugin Output'],
+        synopsis=issue['Description'].replace("_","\_"),
+        description=issue['Description'].replace("_","\_"),
+        rule_query = issue['Rule Query'].replace("_","\_"),
+        expected_result = issue['Expected Result'].replace("_","\_"),
+        actual_result = issue['Actual Result'].replace("_","\_"),
+        # There must be a better way..
+        #cve="        \item \\href{{https://cve.mitre.org/cgi-bin/cvename.cgi?name={0}}}{{{0}}}\n".format("".join(cve for cve in issue['CVE'].split())),
+        host=''.join('        \item %s\n' % host for host in issue['Server']),
+        #see_also=''.join('%%        \\url{%s}\n\n' % see_also for see_also in issue['See Also'].split()),
+        solution=issue['Remediation'].replace("_","\_"),
       )
-      issue_file.close()
+    )
+    issue_file.close()
 
 if __name__ == "__main__":
     main()
